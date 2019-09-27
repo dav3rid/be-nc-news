a38cf15cbbe6b331c54bcf45738c9a66b56e2885
Dave Ridealgh <davecodingstuff@gmail.com>
2019-09-27 08:57:51 +0100

    refactored utils

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,28 +1,20 @@
 exports.formatDates = list => {
-  if (list.length < 1) return [];
-  const newArr = [];
-  list.forEach(obj => {
+  return list.map(obj => {
     const newObj = { ...obj };
-    const currentDate = newObj.created_at;
-    newObj.created_at = new Date(currentDate);
-    newArr.push(newObj);
+    newObj.created_at = new Date(newObj.created_at);
+    return newObj;
   });
-  return newArr;
 };
 
 exports.makeRefObj = (list, keyName = 'title', valueName = 'article_id') => {
-  if (list.length < 1) return {};
-  const refObj = {};
-  list.forEach(obj => {
-    refObj[obj[keyName]] = obj[valueName];
-  });
-  return refObj;
+  return list.reduce((refObj, currentObj) => {
+    refObj[currentObj[keyName]] = currentObj[valueName];
+    return refObj;
+  }, {});
 };
 
 exports.formatComments = (comments, articleRef) => {
-  if (comments.length < 1) return [];
-  const newArr = [];
-  comments.forEach(comment => {
+  return comments.map(comment => {
     const newObj = { ...comment };
     newObj.author = newObj.created_by;
     delete newObj.created_by;
@@ -30,9 +22,7 @@ exports.formatComments = (comments, articleRef) => {
     newObj.article_id = articleRef[newObj.belongs_to];
     delete newObj.belongs_to;
 
-    const currentDate = newObj.created_at;
-    newObj.created_at = new Date(currentDate);
-    newArr.push(newObj);
+    newObj.created_at = new Date(newObj.created_at);
+    return newObj;
   });
-  return newArr;
 };
