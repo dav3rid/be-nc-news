b1d041b175797f9e10b7c923fccec7ee255feae1
Dave Ridealgh <davecodingstuff@gmail.com>
2019-09-27 12:26:31 +0100

    configured for hosting

--- db/data/index.js
+++ db/data/index.js
@@ -2,7 +2,8 @@ const ENV = process.env.NODE_ENV || 'development';
 
 const allData = {
   development: require('./development-data'),
-  test: require('./test-data')
+  test: require('./test-data'),
+  production: require('./development-data')
 };
 
 module.exports = allData[ENV];
