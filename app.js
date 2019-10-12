ef2f79de82d45baee30f90ac67b9f40a3fbf0d4c
Dave Ridealgh <davecodingstuff@gmail.com>
2019-10-12 12:31:39 +0100

    cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const apiRouter = require('./routes/api-router');
 const {
@@ -9,6 +10,7 @@ const {
   handle500s
 } = require('./errors');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
